7c01431dd9e0dc6b7a4acd0b6ae21be75fb4c475
Carlos López Quintanilla <carlos.lopez@psig.es>
2022-02-28 11:53:18 +0100

    Update sql_sector.sql
    Update ubicació schema de la taula parcela

--- config/sql_sector.sql
+++ config/sql_sector.sql
@@ -1,7 +1,7 @@
 SELECT c.codi AS codi, c.descripcio AS descr,
     ST_Area(ST_Intersection(c.geom, p.geom)) AS area_int,
     (ST_Area(ST_Intersection(c.geom, p.geom)) / p.area) * 100 as per_int
-FROM planejament_urba.sectors_urbanistics AS c, carto.parcela as p
+FROM planejament_urba.sectors_urbanistics AS c, cadastre.parcela as p
 WHERE p.ninterno = $ID_VALUE
     AND ST_Intersects(p.geom, c.geom)
     AND (ST_Area(ST_Intersection(c.geom, p.geom)) / p.area) * 100 > 3
